e78c37f08b1203e484651498c9f418eee8bbf0de
Anmol Pathry <43543801+anmolpathry@users.noreply.github.com>
2024-02-16 11:52:18 -0600

    Normalized the json data and displayed in a dataframe. Modified the base url for request to add variable for the name of the fruit.

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,6 +28,14 @@ streamlit.dataframe(fruits_to_show)
 #Display the fruityvice api response
 streamlit.header('Fruityvice Fruit Advice!')
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-#streamlit.text(fruityvice_response)
-streamlit.text(fruityvice_response.json())
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+#This writes the status of the response
+#streamlit.text(fruityvice_response) 
+
+#This writes the data o the screen
+#streamlit.text(fruityvice_response.json()) 
+
+#We normalize the json version of the json and display it as a table
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+streamlit.dataframe(fruityvice_normalized)
+
